cccf68e8015bcff6a7380b38f3dbb9efda861b1a
René Torres <7orresrene@users.noreply.github.com>
2022-01-17 22:12:56 -0500

    Add complex functionality

---
+++ src/components/JobList.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="job-list">
+		<p>Ordered by {{ order }}</p>
+		<ul>
+			<li v-for="job in jobs" :key="job.id">
+				<h2>{{ job.id }} who is a {{ job.title }} in {{ job.location }}</h2>
+				<div class="salary">
+					<p>{{ job.salary }} rupees</p>
+				</div>
+				<div class="description">
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Exercitationem unde ex autem cumque odio, dicta nostrum eligendi
+						amet non. Facilis modi mollitia adipisci velit beatae incidunt
+						temporibus culpa error tenetur.
+					</p>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import Job from "./types/Job.ts/Job";
+import OrderTerm from "./types/OrderTerm";
+
+export default defineComponent({
+	props: {
+		jobs: {
+			required: true,
+			type: Array as PropType<Job[]>,
+		},
+		order: {
+			required: true,
+			type: String as PropType<OrderTerm>,
+		},
+	},
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,66 +1,53 @@
 <template>
-  <div class="app">
-    <h1>{{ jobs[0].id }}</h1>
-    <p>
-      They live in {{ jobs[0].location }}. Their bff is {{ jobs[1].id }}, who
-      makes {{ jobs[1].salary }} per year
-    </p>
-  </div>
+	<div class="app">
+		<header class="btn-container">
+			<button @click="handleClick('title')">Order by title</button>
+			<button @click="handleClick('salary')">Order by salary</button>
+			<button @click="handleClick('location')">Order by location</button>
+		</header>
+		<JobList :jobs="jobs" :order="order" />
+	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, toRefs, ref } from "vue";
+import { defineComponent, reactive, ref } from "vue";
+import JobList from "../components/JobList.vue";
 import Job from "../components/types/Job.ts/Job";
+import OrderTerm from "../components/types/OrderTerm";
 
 export default defineComponent({
-  nama: "App",
-  components: {},
-  setup() {
-    // const state = reactive({
-    //   name: "Link" as string,
-    //   age: 25 as number | string,
-    // });
+	nama: "App",
+	components: { JobList },
+	setup() {
+		const jobs = ref<Job[]>([
+			{
+				title: "developer",
+				location: "Mars",
+				salary: 5,
+				id: " rtorres",
+			},
+			{
+				title: "developer",
+				location: "Mars",
+				salary: 7,
+				id: "phamer",
+			},
+			{
+				title: "bed-warmer",
+				location: "Chunky-Land",
+				salary: 10000000000,
+				id: "cmonkey",
+			},
+		]);
+		const order = ref<OrderTerm>("title");
+		const handleClick = (term: OrderTerm) => {
+			order.value = term;
+		};
 
-    // state.name = "r-dog";
-
-    // return {
-    //   ...toRefs(state),
-    // };
-    // const name = ref<string>("Link");
-    // const age = ref<number | string>(25);
-    // // Or
-    // // const name = ref("Link" as string);
-    // // const age = ref(25 as number | string);
-
-    // name.value = "René";
-    // age.value = "25";
-
-    const jobs = ref<Job[]>([
-      {
-        title: "developer",
-        location: "Mars",
-        salary: 5,
-        id: " rtorres",
-      },
-      {
-        title: "developer",
-        location: "Mars",
-        salary: 7,
-        id: "phamer",
-      },
-      {
-        title: "bed-warmer",
-        location: "Cunky-Land",
-        salary: 10000000000,
-        id: "cmonkey",
-      },
-    ]);
-
-    return { jobs };
-  },
-  methods: {},
+		return { jobs, handleClick, order };
+	},
+	methods: {},
 });
 </script>
 
-<style lang="sass">
-</style>
+<style lang="sass"></style>
